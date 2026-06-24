31c38bf4bbfbb0467416595d2d577ddc523aaeeb
Zee-cloudafk <chestazayyan843@gmail.com>
2026-06-24 15:50:12 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,18 @@
 local Games = {
-    [286090429] = "arsenal"
+    [286090429] = "arsenal",
+    [97598239454123] = "grow_a_garden_2" 
 }
 
 local scriptName = Games[game.PlaceId]
 
 if not scriptName then
-    warn("Game not supported")
+    warn("Game not supported by Zee-Hub")
     return
 end
 
 local BaseURL = "https://raw.githubusercontent.com/Zee-cloudafk/Zee-Hub/main/games/"
+
+-- Pesan di console executor agar kamu tahu script berhasil terdeteksi
+print("Mencoba memuat script untuk: " .. scriptName)
+
 loadstring(game:HttpGet(BaseURL .. scriptName .. ".lua"))()
